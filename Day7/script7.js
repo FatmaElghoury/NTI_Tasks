6876ce499819ac28fd3125fa4d049c2e7787e956
Fatma Elghoury <fatmaelghoury@gmail.com>
2026-09-01 16:19:13 -0700

    Dat7

--- Day7/script7.js
+++ Day7/script7.js
@@ -298,12 +298,12 @@
 // console.log(obj2);
 
 //==========32.==================
-const students = [ 
-    { name: "Omar", grade: 80 }, 
-    { name: "Mona", grade: 90 }, 
-    { name: "Ali", grade: 70 } 
-];
+// const students = [ 
+//     { name: "Omar", grade: 80 }, 
+//     { name: "Mona", grade: 90 }, 
+//     { name: "Ali", grade: 70 } 
+// ];
 
-for(const { name, grade } of students){
-    console.log(`${name} scored ${grade}`);
-}
+// for(const { name, grade } of students){
+//     console.log(`${name} scored ${grade}`);
+// }
