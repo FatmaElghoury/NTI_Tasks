8dd335f2e71a1aeb39c11849b87ee8557b67b097
Fatma Elghoury <fatmaelghoury@gmail.com>
2026-09-01 16:23:38 -0700

    Update Day7 script

--- Day7/script7.js
+++ Day7/script7.js
@@ -6,7 +6,7 @@
 // test();//print fatma
 // console.log(name);//print fatma
 
-//==========2.===================
+// ==========2.===================
 // function test2() {
 //     "use strict"
 // try{
@@ -20,7 +20,7 @@
 // test2();//fatma
 // console.log(x);//error
 
-//==========3.===================
+// ==========3.===================
 // function test3() {
 //     "use strict"
 //     var localVar = "test"
@@ -38,11 +38,11 @@
 // console.log(obj);
 // delete obj.name;
 // console.log(obj);
-//==========4.==================
+// ==========4.==================
 // console.log(x);//undefined
 //  var x = 10; 
 //  console.log(x);//10
-//==========5.==================
+// ==========5.==================
 // // //Case 1 
 //  sayHi();//Hi
 //  function sayHi() { 
@@ -51,10 +51,10 @@
 //  //sayBye();//not function
 //   var sayBye = function () {
 //      console.log("Bye"); };
-//==========6.==================
+// ==========6.==================
 // console.log(a);
 //  let a = 5;//Error let==> مش بيتعمله hoisting
-//==========7.==================
+// ==========7.==================
 // var n = 1;
 //  function demo() {
 //       console.log(n);//undefined
@@ -63,7 +63,7 @@
 //     }
 //        demo();
 //         console.log(n);//1
-//==========8.==================
+// ==========8.==================
 // function test4(){
 
 
@@ -76,7 +76,7 @@
 // }
 // test4();
 // console.log(s);//error
-//==========9.==================
+// ==========9.==================
 // function test4(){
 
 
@@ -89,12 +89,12 @@
 // }
 // test4();
 
-//==========10.==================
+// ==========10.==================
 // var num=5;
 // var num= "fstma";
 // let x=4;
 // let x="fatma";//error
-//==========11.==================
+// ==========11.==================
 // const student = {name:"fatma",age:20,city:"cairo"};
 // console.log(student);
 
@@ -109,7 +109,7 @@
 // }
 // catch(error){console.log("Error",error.message);//assign to const
 // }
-//==========12.==================
+// ==========12.==================
 // const nums = [1,2,3]
 // console.log(nums);
 
@@ -118,18 +118,18 @@
 // nums[0]=10;//allowed
 // console.log(nums);
 // nums=[20,30,40];//not allowed
-//==========13.==================
+// ==========13.==================
 // var a; //valid
 // let b; //valid
 // const c; //not valid===>must assign value 
-//==========14.==================
+// ==========14.==================
 //  var g1 = "var global";
 //  let g2 = "let global";
 //  const g3 = "const global";
 //  console.log(window.g1);//exist
 //  console.log(window.g2);//not exist
 //  console.log(window.g3);//not exist
-//==========15.==================
+// ==========15.==================
 // const handlers = {};
 // for (let i = 0; i < 3; i++) { //let is block scope, var is function scope
 //     handlers["fn" + i] = function () {
@@ -138,7 +138,7 @@
 // }
 // console.log(handlers.fn0());
 // console.log(handlers.fn2());
-//==========16.==================
+// ==========16.==================
 // function welcome(name) {
 //      return "Welcome, " + name + "!"; }
 
@@ -147,16 +147,16 @@
 //  console.log(welcome("fatma"));
 
 
-//==========17.==================
+// ==========17.==================
 // var fullInfo = (first, last, age)  => `${first} ${last} is ${age} years old`;
 // console.log(fullInfo("fatma","Elghoury",21));
 
-//==========18.==================
+// ==========18.==================
 // var multiply = (a,b)=>a*b;
 // var sum = (a,b)=>`${a} + ${b} = ${a+b}`
 // console.log(multiply(2,4));
 // console.log(sum(2,4));
-//==========19.==================
+// ==========19.==================
 // const product = {
 //     title: "Laptop",
 //     price: 15000,
@@ -168,12 +168,12 @@
 // console.log(price);
 // console.log(inStock);
 
-//==========20.==================
+// ==========20.==================
 // var arr = ["HTML", "CSS", "JS", "React"];
 // var [item1,item2]=arr;
 // console.log(item1);
 // console.log(item2);
-//==========21.==================
+// ==========21.==================
 // function  greet (name = "Guest", message = "Hello")
 // {
 //     return `${message}, ${name}`;
@@ -181,7 +181,7 @@
 // console.log(greet("fatma","welcome"));
 // console.log(greet("fatma"));
 // console.log(greet());
-//==========22.==================
+// ==========22.==================
 // function sumAll(...numbers){
 //     var total=0;
 //     for(let i=0;i<numbers.length;i++){
@@ -193,7 +193,7 @@
 // console.log(sumAll(10, 20, 30, 40));
 
 
-//==========23.==================
+// ==========23.==================
 // var arr1 =[1,2];
 // var arr2 =[3,4,5];
 // var merge =[...arr1,...arr2];
@@ -204,19 +204,19 @@
 // console.log(arr4);
 // console.log(arr4copy);
 
-//==========24.==================
+// ==========24.==================
 // const user = { name: "Sara", age: 22 }; 
 // const contact = { email: "sara@nti.com", age: 23 };
 // var merged={...contact,...user};//take the age of first item
 // console.log(merged);
 
-//==========25.==================
+// ==========25.==================
 // const values = [2, 4, 6, 8];
 //  function total(a, b, c, d) {
 //      return a + b + c + d; }
 //      console.log(total(...values));
 
-//==========26.==================
+// ==========26.==================
 // let person1 = { name: "Ali", child: { age: 5 } };
 // //  let person2 = person1;//not copy,bpth of two object see the same reference in memory
 // //   person2.name = "Omar"; //person1.name ="Omar"
@@ -225,7 +225,7 @@
 // let person2={...person1};//right copy
 // person2.name = "Omar"; 
 // console.log(person1.name);//Ali 
-//==========27.==================
+// ==========27.==================
 // const original = {
 //     name: "Mona",
 //     details: { city: "Cairo" }
@@ -237,7 +237,7 @@
 // console.log(original);
 // console.log(shallow_copy);
 
-//==========28.==================
+// ==========28.==================
 // const original = {
 //     name: "Mona",
 //     details: { city: "Cairo" }
@@ -248,7 +248,7 @@
 // shallow_copy.details.city = "giza";//original no affect
 // console.log(original);
 //  console.log(shallow_copy);
-//==========29.==================
+// ==========29.==================
 // var obj = {
 //     name: "Ahmed",
 //     age: 26,
@@ -261,7 +261,7 @@
 // console.log(`${reternObj}: ${typeof(reternObj)}`);
 
 // localStorage.clear();
-//==========30.==================
+// ==========30.==================
 // const  APP_CONFIG={
 //      name :"mvc",
 //       version:5,
@@ -281,7 +281,7 @@
 // catch(error){console.log("error",error.message);
 // };
 
-//==========31.==================
+// ==========31.==================
 // function  createCard(title, price = 0, ...tags){
 //     var obj= {
 //         Title : title,
@@ -297,7 +297,7 @@
 // var obj2= createCard("Laptop",15000)
 // console.log(obj2);
 
-//==========32.==================
+// ==========32.==================
 // const students = [ 
 //     { name: "Omar", grade: 80 }, 
 //     { name: "Mona", grade: 90 }, 
@@ -307,3 +307,4 @@
 // for(const { name, grade } of students){
 //     console.log(`${name} scored ${grade}`);
 // }
+
